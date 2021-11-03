8f994bef4caf84fb9ee3d16e0610584187be405a
VasiliyYurckevich <newskill228@gmail.com>
2021-11-03 17:00:50 +0300

    1.2 compare of Strings and names of final var

--- src/com/company/NumberToTitle.java
+++ src/com/company/NumberToTitle.java
@@ -8,9 +8,11 @@ import java.util.*;
 
 
 public class NumberToTitle  {
-    private final static char ONEENDING= '1',twoEnding='2',threeEnding='3',fourEnding ='4';
-    private final static String FULLZERO = "000";
-    private final static String minus = "-";
+    private final static char ONEENDING= '1',TWOENDING='2',threeEnding='3',fourEnding ='4';
+    private final static String ZERO = "0";
+
+
+    //private final static String minus = "-";
     private StringBuffer composition = new StringBuffer();
 
 
@@ -60,7 +62,7 @@ public class NumberToTitle  {
     public  NumberToTitle(BigInteger numIn){
         getDataStorage(numIn);
         checkingForNegativity(dataStorage.get(0));
-        if(dataStorage.size() == 1 && dataStorage.get(0).equals("0")){
+        if(dataStorage.size() == 1 && dataStorage.get(0).equals(ZERO)){
             composition.append("ноль");
         }
         for (int i=0;i<dataStorage.size();i++){
@@ -70,7 +72,7 @@ public class NumberToTitle  {
                 subs.insert(0, '0');
             }
             dataStorage.set(i,subs.toString());
-            if(dataStorage.get(i).equals(FULLZERO) == false) {
+            if(dataStorage.get(i).equals(ZERO) == false) {
                 if (dataStorage.size() == i + 1) {
                     convertor.convertorUnit(dataStorage.get(i));
                     composition.append(convertor.getName() + " ");
@@ -79,7 +81,7 @@ public class NumberToTitle  {
                     if (dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == ONEENDING &&
                         dataStorage.get(i).charAt(dataStorage.get(i).length() - 2) != ONEENDING) {
                         composition.append(convertor.getName() + " " + degree.get(dataStorage.size() - i - 1) + "а ");
-                    } else if ((dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == twoEnding ||
+                    } else if ((dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == TWOENDING ||
                             dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == threeEnding ||
                             dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == fourEnding) &&
                             dataStorage.get(i).charAt(dataStorage.get(i).length() - 2) != ONEENDING
@@ -95,7 +97,7 @@ public class NumberToTitle  {
                     if (dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == ONEENDING &&
                         dataStorage.get(i).charAt(dataStorage.get(i).length() - 2) != ONEENDING) {
                         composition.append(convertor.getName() + " " + degree.get(dataStorage.size() - i - 1) + " ");
-                    } else if ((dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == twoEnding ||
+                    } else if ((dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == TWOENDING ||
                             dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == threeEnding ||
                             dataStorage.get(i).charAt(dataStorage.get(i).length() - 1) == fourEnding ) &&
                             dataStorage.get(i).charAt(dataStorage.get(i).length() - 2) != ONEENDING
